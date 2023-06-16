a7157f88e5bb0681d151e5a4aef74503ef580826
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-06-16 10:46:59 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    var answer = '';
+    num = Math.floor(s.length / 2)
+    answer = (s.length % 2 === 0)? s.slice(num - 1, num + 1): s[num];  
+    
+    return answer;
+}
