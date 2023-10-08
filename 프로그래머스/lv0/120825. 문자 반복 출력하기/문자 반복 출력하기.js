5efbdcc87afa54a6b12ed325de495a75c82bef68
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-09 08:04:43 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,6 +1,6 @@
 function solution(my_string, n) {
     const newArray = [] 
-    for (a of my_string.split("")) {
+    for (a of [...my_string]) {
         newArray.push(a.repeat(n))
     }
     return newArray.join("");
