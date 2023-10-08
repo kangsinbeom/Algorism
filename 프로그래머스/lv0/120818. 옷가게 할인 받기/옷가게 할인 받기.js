831482074f4224db6bad723fa079c0457c44a2bf
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-09 07:38:36 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,12 @@
 function solution(price) {
     if (price >= 100000 && price < 300000) {
-        return Math.floor(price * .95)
+        price = Math.floor(price * 0.95)
     } else if (price >= 300000 && price < 500000) {
-        return Math.floor(price * .9)
+        price = ~~(price * 0.9)
     } else if (price >= 500000) {
-        return Math.floor(price * .8)
+        price = price - (price * 0.2)
     } else {
-        return price
+        price = price
     }
+    return price;
 }
