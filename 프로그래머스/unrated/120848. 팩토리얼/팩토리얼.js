4fdd5d5d7e9725bf2e3ab2defb853992ce523c2a
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-23 11:24:26 +0900

    [level unrated] Title: 팩토리얼, Time: 2.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
@@ -1,13 +1,11 @@
 function solution(n) {
-    let a = 1
-    let i = 1
+    let a = 1, i = 1
     while (true) {
         if (n < a) {
             console.log(a, i)
             return i - 2    
         }
         a *= i 
-        i++
-        console.log(a, i)
+        ++i
     }
 }
