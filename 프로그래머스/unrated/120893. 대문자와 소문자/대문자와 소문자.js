19a951d3ff39ebe4503f8b3211be774e3abecb51
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-24 13:12:30 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,4 +1,4 @@
 function solution(my_string) {
-    var answer = [...my_string].map(x => x.charCodeAt(0)).map(x=> x< 97 ? x + 32 : x -32).map(x=> String.fromCharCode(x)).join("");
+    var answer = [...my_string].map(x => x.toLowerCase() === x ? x.toUpperCase() : x.toLowerCase()).join("")
     return answer;
 }
