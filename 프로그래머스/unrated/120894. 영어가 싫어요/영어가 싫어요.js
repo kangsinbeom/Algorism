396064cd2178ff2a7754c3d69a5b75bb42e707ee
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-27 17:25:35 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,9 +1,5 @@
 function solution(numbers) {
     const array1 = "zero one two three four five six seven eight nine".split(" ")
-    let answer = numbers
-    for (let i = 0; i < array1.length; i++) {
-        answer = answer.split(array1[i]).join(i)
-    }
-    
-    return Number(answer);
+    .reduce((a, b, i) => a.replaceAll(b, i) , numbers)
+    return Number(array1);
 }
