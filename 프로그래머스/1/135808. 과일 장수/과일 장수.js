1251487dae022ffe007cd553e8f1544490d07f80
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-11-23 08:15:11 +0900

    [level 1] Title: 과일 장수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,19 +1,14 @@
 function solution(k, m, score) {
-    if(score.length < m){
-        return 0;
-    }
-    
-    score.sort((a, b) => a - b);
-    
-    let total = 0;
-    
-    while(score.length >= m){
-        const box = score.splice(score.length - m, m);
-        
-        const prize = m * box[0];
-        
-        total += prize;
-    }
-    
-    return total;
+    var answer = 0
+    const box = Math.floor(score.length / m)
+    const obj = {}
+    score.forEach((x)=>{
+        if (obj[x]) {
+            return obj[x] ++    
+        }
+        return obj[x] = 1
+    })    
+    const keys = Object.keys(obj)
+    console.log(box, keys)
+    return answer;
 }
