78e5c1083bc206b2efe0afa0a02bd869e22e2c5c
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2025-01-16 13:37:37 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.py
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.py
@@ -1,19 +1,12 @@
 import sys
-# 뜨거운 붕어빵 브론즈 4
-
-"""
-문제 해설
-주어진 입력값에 대해서 좌 우로 뒤집힌 모양으로 출력하는 것이다.
-"""
-
-N,M = map(int, input().split(" "))
 
 def solution_1(string):
-    
     return "".join(reversed(list(string)))
 
+N, M = map(int, input().split())
 
 for _ in range(N):
-    string = input()
+    string = sys.stdin.readline().strip()
+    if not string:
+        continue
     print(solution_1(string))
-# Runtime Error
