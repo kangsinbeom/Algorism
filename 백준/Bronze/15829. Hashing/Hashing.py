8977ee460ba39f5b828656c9a356d232502c4125
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2025-01-21 21:17:19 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,13 +1,12 @@
-
-
-def solution():
-    len = int(input())
-    string = input()
-    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    sum = 0
-    for i in range(len):
-        index = alpha.index(string[i])
-        sum += (index + 1) * (31 ** i)         
-    return sum
-
-print(solution())
+L = int(input())
+M = 1234567891
+r = 31
+user_input = input()
+ 
+answer = 0
+ 
+for i in range(len(user_input)):
+    num = ord(user_input[i]) - 96
+    answer += num * (r ** i)
+ 
+print(answer % M)
