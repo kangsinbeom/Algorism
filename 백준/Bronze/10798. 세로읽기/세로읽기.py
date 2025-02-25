d7481fb3e9b62db60ec9978ef870d09b4292a785
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2025-02-25 11:02:29 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,19 +1,14 @@
-def 라인맞추기(str, maxLen):
-    if len(str) < maxLen:
-        return str + " "*(maxLen - len(str)) 
-    return str
+import sys
 def solution():
     result = ""
     lines = []
     for _ in range(5):
-        line = input()
-        lines.append(line)
+        lines.append(list(sys.stdin.readline().rstrip()))   
     maxLen = max([len(line) for line in lines])
-    lines = [라인맞추기(line, maxLen) for line in lines]
-    
     for i in range(maxLen):
         for j in range(5):
-            if lines[j][i] != " ":
-                result += lines[j][i]
+            if (len(lines[j]) < i + 1):
+                continue
+            result += lines[j][i]
     return result
 print(solution())
